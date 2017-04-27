8405deccebdbec73b65a2d3bab042e6eb15ae6df
DchyunFF1204 <871756795@qq.com>
2017-04-27 20:23:30 +0800

    a

--- ppb-wxservice/src/main/java/com/wx/WxApplication.java
+++ ppb-wxservice/src/main/java/com/wx/WxApplication.java
@@ -2,7 +2,6 @@ package com.wx;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author daizy
