3c1d56bf8adf8dfbd0aafd1878c93ed5b4eabe9b
DchyunFF1204 <871756795@qq.com>
2017-05-19 08:51:55 +0800

    支付宝app支付接口

--- springboot-alipay/src/main/java/com/ppb/AlipayController.java
+++ springboot-alipay/src/main/java/com/ppb/AlipayController.java
@@ -2,12 +2,11 @@ package com.ppb;
 
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
+import com.alipay.api.domain.AlipayTradeAppPayModel;
 import com.alipay.api.internal.util.AlipaySignature;
-import com.alipay.api.request.AlipayDataDataserviceBillDownloadurlQueryRequest;
-import com.alipay.api.request.AlipayTradeQueryRequest;
-import com.alipay.api.request.AlipayTradeRefundRequest;
-import com.alipay.api.request.AlipayTradeWapPayRequest;
+import com.alipay.api.request.*;
 import com.alipay.api.response.AlipayDataDataserviceBillDownloadurlQueryResponse;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
 import com.alipay.api.response.AlipayTradeQueryResponse;
 import com.alipay.api.response.AlipayTradeRefundResponse;
 import com.ppb.client.AlipayConfig;
@@ -40,6 +39,7 @@ public class AlipayController {
     private AlipayConfig alipayConfig;
 
     /**
+     * 支付宝wap支付
      * @param out_trade_no
      * @param total_amount
      * @param subject
@@ -58,6 +58,34 @@ public class AlipayController {
                 "    \"product_code\":\"QUICK_WAP_PAY\"" +         // 销售码
                 "  }");//填充业务参数
         return alipayClient.pageExecute(alipayRequest).getBody(); //调用SDK生成表单
+    }
+
+    /**
+     * 支付宝app支付
+     * @param body
+     * @param subject
+     * @param out_trade_no
+     * @param total_amount
+     * @return
+     * @throws AlipayApiException
+     */
+    @RequestMapping("/alipayTradeAppPay")
+    public String alipayTradeAppPay(String body,String subject,String out_trade_no,String total_amount) throws AlipayApiException {
+        //实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.app.pay
+        AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+        //SDK已经封装掉了公共参数，这里只需要传入业务参数。以下方法为sdk的model入参方式(model和biz_content同时存在的情况下取biz_content)。
+        AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
+        model.setBody(body);
+        model.setSubject(subject);
+        model.setOutTradeNo(out_trade_no);
+        model.setTimeoutExpress("30m");
+        model.setTotalAmount(total_amount);
+        model.setProductCode("QUICK_MSECURITY_PAY");
+        request.setBizModel(model);
+        request.setNotifyUrl("http://domain.com/alipayNotifyTradeWapPay");
+        AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
+        System.out.println(response.getBody());//就是orderString 可以直接给客户端请求，无需再做处理。
+        return response.getBody();
     }
 
     /**
@@ -96,7 +124,6 @@ public class AlipayController {
         }
         return "success";
     }
-
 
     /**
      * 查询交易单
