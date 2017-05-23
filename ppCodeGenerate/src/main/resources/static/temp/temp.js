75f4efd07cbba02b67292398b17c10d818317a6d
DchyunFF1204 <871756795@qq.com>
2017-05-23 17:45:46 +0800

    代码结构优化

--- ppCodeGenerate/src/main/resources/static/temp/temp.js
+++ ppCodeGenerate/src/main/resources/static/temp/temp.js
@@ -1,30 +1,30 @@
-<#noparse>$(</#noparse>function () {
+$(function () {
     // 初始化条件框
-    ${lowClassName}Js.initParamForm();
+    tempJs.initParamForm();
     //初始化Table
-    ${lowClassName}Js.initTable();
+    tempJs.initTable();
     // 绑定静态button事件
-    ${lowClassName}Js.initButton();
+    tempJs.initButton();
 });
 
-var ${lowClassName}Js = {
-    queryTableLink : "/${lowClassName}/select${className}PgByModel",   // 查询列表server url
-    queryDataByPriId : "/${lowClassName}/get${className}ById",// 主键查询 server url
-    saveData : "/${lowClassName}/save${className}",   // 数据保存 server url
+var tempJs = {
+    queryTableLink : "/queryPg",   // 查询列表server url
+    queryDataByPriId : "/queryById",// 主键查询 server url
+    saveData : "/save",   // 数据保存 server url
     // 初始化条件栏位
     initParamForm : function(){
-        <#noparse>$(</#noparse>'#usertype').selectpicker({
+        $('#usertype').selectpicker({
             'selectedText': 'cat'
         });
 
-        <#noparse>$(</#noparse>"#form_datetime_start").datetimepicker({
+        $("#form_datetime_start").datetimepicker({
             format: "yyyy-mm-dd hh:ii:ss",
             autoclose: true,
             todayBtn: true,
             pickerPosition: "bottom-left",
             minuteStep: 10
         });
-        <#noparse>$(</#noparse>"#form_datetime_end").datetimepicker({
+        $("#form_datetime_end").datetimepicker({
             format: "yyyy-mm-dd hh:ii:ss",
             autoclose: true,
             todayBtn: true,
@@ -37,32 +37,32 @@ var ${lowClassName}Js = {
         var oTableInit = new Object();
         //初始化Table
         oTableInit.Init = function () {
-        <#noparse>$(</#noparse>'#tb_departments').bootstrapTable({
-            url: ${lowClassName}Js.getQueryTableLink(), //请求后台的URL（*）
-            method: 'post', //请求方式（*）
-            //toolbar: '#toolbar', //工具按钮用哪个容器
-            striped: true, //是否显示行间隔色
-            cache: false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-            pagination: true, //是否显示分页（*）
-            sortable: false, //是否启用排序
-            sortOrder: "asc", //排序方式
-            queryParams: oTableInit.queryParams,//传递参数（*）
-            sidePagination: "server", //分页方式：client客户端分页，server服务端分页（*）
-            pageNumber:1, //初始化加载第一页，默认第一页
-            pageSize: 10, //每页的记录行数（*）
-            pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）
-            search: false, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-            strictSearch: false,
-            showColumns: true, //是否显示所有的列
-            showRefresh: true, //是否显示刷新按钮
-            minimumCountColumns: 2, //最少允许的列数
-            clickToSelect: true, //是否启用点击选中行
-            height: "auto", //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-            uniqueId: "ID", //每一行的唯一标识，一般为主键列
-            showToggle:true, //是否显示详细视图和列表视图的切换按钮
-            cardView: false, //是否显示详细视图
-            detailView: false, //是否显示父子表
-            columns: [{
+            $('#tb_departments').bootstrapTable({
+                url: tempJs.getQueryTableLink(), //请求后台的URL（*）
+                method: 'post', //请求方式（*）
+                //toolbar: '#toolbar', //工具按钮用哪个容器
+                striped: true, //是否显示行间隔色
+                cache: false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+                pagination: true, //是否显示分页（*）
+                sortable: false, //是否启用排序
+                sortOrder: "asc", //排序方式
+                queryParams: oTableInit.queryParams,//传递参数（*）
+                sidePagination: "server", //分页方式：client客户端分页，server服务端分页（*）
+                pageNumber:1, //初始化加载第一页，默认第一页
+                pageSize: 10, //每页的记录行数（*）
+                pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）
+                search: false, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+                strictSearch: false,
+                showColumns: true, //是否显示所有的列
+                showRefresh: true, //是否显示刷新按钮
+                minimumCountColumns: 2, //最少允许的列数
+                clickToSelect: true, //是否启用点击选中行
+                height: "auto", //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+                uniqueId: "ID", //每一行的唯一标识，一般为主键列
+                showToggle:true, //是否显示详细视图和列表视图的切换按钮
+                cardView: false, //是否显示详细视图
+                detailView: false, //是否显示父子表
+                columns: [{
                     checkbox: true
                 }, {
                     field: 'Name',
@@ -80,14 +80,14 @@ var ${lowClassName}Js = {
                     field: 'use',
                     title: '操作',
                     formatter: function (value, rec) {
-                        var ops = ${lowClassName}Js.setButtons([
+                        var ops = tempJs.setButtons([
                             {
                                 type: 'edit',
                                 title:'编辑',
                                 param : rec.id,
                                 selector : 'btn btn-sm blue',
                                 icon : 'glyphicon glyphicon-pencil',
-                                callFunction : '${lowClassName}Js.openAddOrEdieModal(this)'
+                                callFunction : 'tempJs.openAddOrEdieModal(this)'
                             },
                             {
                                 type: 'del',
@@ -95,21 +95,21 @@ var ${lowClassName}Js = {
                                 param : rec.id,
                                 selector : 'btn btn-sm blue',
                                 icon : 'glyphicon glyphicon-minus',
-                                callFunction : '${lowClassName}Js.removeData(this)'
+                                callFunction : 'tempJs.removeData(this)'
                             }
                         ]);
                         return ops;
-                     }
+                    }
                 }]
             });
         };
-        //得到查询的参数
+        //查询的参数
         oTableInit.queryParams = function (params) {
             var temp = { //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
                 'page': params.offset / params.limit + 1,
                 'rows': params.limit
-             };
-            var param = <#noparse>$(</#noparse>'#formSearch').serializeJson();
+            };
+            var param = $('#formSearch').serializeJson();
             temp = $.extend(temp, param);
             return temp;
         };
@@ -126,28 +126,28 @@ var ${lowClassName}Js = {
     // 初始化页面静态button事件
     initButton : function () {
         // 查询
-        <#noparse>$(</#noparse>'.js-table-query').on('click', function () {
-            ${lowClassName}Js.queryDataTable();
+        $('.js-table-query').on('click', function () {
+            tempJs.queryDataTable();
         });
         // 新增
-        <#noparse>$(</#noparse>'.js-table-add').on('click', function () {
-            ${lowClassName}Js.openAddOrEdieModal();
+        $('.js-table-add').on('click', function () {
+            tempJs.openAddOrEdieModal();
         });
     },
     // 获取查询链接
     getQueryTableLink :function () {
-        return ${lowClassName}Js.queryTableLink+"?rn="+Math.random();
+        return tempJs.queryTableLink+"?rn="+Math.random();
     },
     // 查询table data
     queryDataTable:function () {
-        <#noparse>$(</#noparse>'#tb_departments').bootstrapTable('refresh' , {
+        $('#tb_departments').bootstrapTable('refresh' , {
             url: getQueryLink(),
-            query: <#noparse>$(</#noparse>'#formSearch').serialize()
+            query: $('#formSearch').serialize()
         })
     },
     // 修改数据
     openAddOrEdieModal : function (_this) {
-        <#noparse>$(</#noparse>'#js_table_modal').modal('show');
+        $('#js_table_modal').modal('show');
     },
     // 删除数据
     removeData : function (_this) {
@@ -155,16 +155,16 @@ var ${lowClassName}Js = {
             if (!e) {
                 return;
             }
-            var useParam = <#noparse>$(</#noparse>_this).data('param');
-            ${lowClassName}Js.saveData(useParam);
+            var useParam = $(_this).data('param');
+            tempJs.saveData(useParam);
         });
     },
     // 保存数据
     saveData : function (useParam) {
         // ajax 请求
-        <#noparse>$</#noparse>.post(${lowClassName}Js.saveData, useParam, function (result) {
+        $.post(tempJs.saveData, useParam, function (result) {
             if(result.success){
-                ${lowClassName}Js.queryDataTable();
+                tempJs.queryDataTable();
             }else{
                 Ewin.alert(result.message);
             }
--- ppCodeGenerate/src/main/resources/templates/code/temp.ftl
+++ ppCodeGenerate/src/main/resources/templates/code/temp.ftl
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="static/assets/global/css/plugins.min.css">
     <link rel="stylesheet" href="static/assets/global/plugins/bootstrap-table/bootstrap-table.min.css">
     <link rel="stylesheet" href="static/assets/global/plugins/bootstrap-select/css/bootstrap-select.min.css">
-
 </head>
 <body>
 <div class="container-fluid">
@@ -61,9 +60,9 @@
                         </div>
 
                         <label class="control-label col-sm-1" for="txt_search_statu" style="padding-top: 17px;">时间</label>
-                        <div class="col-sm-4" style="padding-top: 10px;">
+                        <div class="col-sm-5" style="padding-top: 10px;">
                             <div class="input-group date form_datetime" data-date-format="yyyy-mm-dd hh:ii:ss">
-                                <input type="text" size="24" readonly class="form-control" id="form_datetime_start" >
+                                <input type="text" size="28" readonly class="form-control" id="form_datetime_start" >
                                 <span class="input-group-btn">
                                     <button class="btn default date-set" type="button">
                                         <i class="fa fa-calendar"></i>  至
@@ -79,10 +78,10 @@
                         </div>
 
                         <div class="col-sm-7 col-md-offset-5" style="margin-top:15px">
-                            <button id="btn_query" type="button" class="btn btn btn-primary">
+                            <button id="btn_query" type="button" class="btn btn btn-primary js-table-query">
                                 <span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>查询
                             </button>
-                            <button id="btn_add" type="button" class="btn btn btn-primary">
+                            <button id="btn_add" type="button" class="btn btn btn-primary  js-table-add">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>新增
                             </button>
                         </div>
@@ -90,18 +89,25 @@
                 </form>
             </div>
         </div>
-
-        <div id="toolbar" >
-            <button id="btn_edit" type="button" class="btn btn btn-primary">
-                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>修改
-            </button>
-
-            <button id="btn_delete" type="button" class="btn btn btn-primary">
-                <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>删除
-            </button>
-        </div>
         <table id="tb_departments" class="table table-striped table-hover"></table>
+    </div>
+</div>
 
+<div class="modal fade draggable " id="js_table_modal"  data-backdrop="static" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="width: 50%;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="hTitle">新增or编辑操作</h4>
+            </div>
+            <div class="modal-body" tabindex="2" style="outline: none;">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn default glyphicon glyphicon-share-alt" data-dismiss="modal"><i class="icon-remove"></i>关闭</button>
+                <button type="button" class="btn btn-primary glyphicon glyphicon-ok-circle" ><i class="icon-remove"></i>保存</button>
+            </div>
+        </div>
     </div>
 </div>
 <script src="static/assets/global/plugins/jquery.min.js"></script>
@@ -111,113 +117,6 @@
 <script src="static/assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
 <script src="static/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
 <script src="static/js/main/tool.js"></script>
-<script>
-
-    $('#usertype').selectpicker({
-        'selectedText': 'cat'
-    });
-
-    $("#form_datetime_start").datetimepicker({
-        format: "yyyy-mm-dd hh:ii:ss",
-        autoclose: true,
-        todayBtn: true,
-        startDate: "2013-02-14 10:00:01",
-        pickerPosition: "bottom-left",
-        minuteStep: 10
-    });
-    $("#form_datetime_end").datetimepicker({
-        format: "yyyy-mm-dd hh:ii:ss",
-        autoclose: true,
-        todayBtn: true,
-        startDate: "2013-02-14 10:00:01",
-        pickerPosition: "bottom-left",
-        minuteStep: 10
-    });
-
-    $(function () {
-        //1.初始化Table
-        var oTable = new TableInit();
-        oTable.Init();
-        //2.初始化Button的点击事件
-        var oButtonInit = new ButtonInit();
-        oButtonInit.Init();
-
-    });
-
-
-    var TableInit = function () {
-        var oTableInit = new Object();
-        //初始化Table
-        oTableInit.Init = function () {
-            $('#tb_departments').bootstrapTable({
-                url: '/Home/GetDepartment', //请求后台的URL（*）
-                method: 'get', //请求方式（*）
-                //toolbar: '#toolbar', //工具按钮用哪个容器
-                striped: true, //是否显示行间隔色
-                cache: false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-                pagination: true, //是否显示分页（*）
-                sortable: false, //是否启用排序
-                sortOrder: "asc", //排序方式
-                queryParams: oTableInit.queryParams,//传递参数（*）
-                sidePagination: "server", //分页方式：client客户端分页，server服务端分页（*）
-                pageNumber:1, //初始化加载第一页，默认第一页
-                pageSize: 10, //每页的记录行数（*）
-                pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）
-                search: false, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-                strictSearch: false,
-                showColumns: true, //是否显示所有的列
-                showRefresh: true, //是否显示刷新按钮
-                minimumCountColumns: 2, //最少允许的列数
-                clickToSelect: true, //是否启用点击选中行
-                height: "auto", //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-                uniqueId: "ID", //每一行的唯一标识，一般为主键列
-                showToggle:true, //是否显示详细视图和列表视图的切换按钮
-                cardView: false, //是否显示详细视图
-                detailView: false, //是否显示父子表
-                columns: [{
-                    checkbox: true
-                }, {
-                    field: 'Name',
-                    title: '参数'
-                }, {
-                    field: 'ParentName',
-                    title: '参数'
-                }, {
-                    field: 'Level',
-                    title: '参数'
-                }, {
-                    field: 'Desc',
-                    title: '参数'
-                }, {
-                    field: 'use',
-                    title: '操作'
-                }]
-            });
-        };
-
-        //得到查询的参数
-        oTableInit.queryParams = function (params) {
-            var temp = { //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
-                limit: params.limit, //页面大小
-                offset: params.offset, //页码
-                departmentname: $("#txt_search_departmentname").val(),
-                statu: $("#txt_search_statu").val()
-            };
-            return temp;
-        };
-        return oTableInit;
-    };
-
-
-    var ButtonInit = function () {
-        var oInit = new Object();
-        var postdata = {};
-
-        oInit.Init = function () {
-            //初始化页面上面的按钮事件
-        };
-        return oInit;
-    };
-</script>
+<script src="static/js/temp/temp.js"></script>
 </body>
 </html>
